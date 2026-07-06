6c52afb12434ca3e1f70a8bdcb1735ec04fd716c
BozhenaOliinyk <bozhenaolijnyk01@gmail.com>
2026-07-06 12:05:17 +0300

    bugfix-CL-73-correct-format

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -11,8 +11,8 @@ module "vpc" {
   #public subnet for load balancer and jenkins,
   public_subnets = ["10.0.101.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
